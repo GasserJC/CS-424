ebcf108fb2778d9106a1d1c395dc9c15c4012dfc
root <jacobgasser17@gmail.com>
2021-02-19 20:10:20 +0000

    pcb_1 finished

--- PCB_V1.py
+++ PCB_V1.py
@@ -1,13 +1,19 @@
 #dependencies
-import llist 
+import llist
 from llist import sllist, sllistnode
 
 class PCB1:
-    def __init__(self):
-        print("Self")
+	def __init__(self, parent):
+		self.parent = parent
+		self.children = sllist([])
+	
+	def create(self, p):
+        	children.append(p)
+	
+	def destroy(self, p):
+		for i in range(0, children.size()):
+			node = children.nodeat(i)
+			if(node.value == p):
+				children.remove(children.nodeat(i))
 
-    def create(self):
-        print("Self")
 
-    def destroy(self):
-        print("Self")
--- PCB_V2.py
+++ PCB_V2.py
@@ -1,9 +1,2 @@
 class PCB2:
-    def __init__(self):
-        print("Self")
 
-    def create(self):
-        print("Self")
-
-    def destroy(self):
-        print("Self")
