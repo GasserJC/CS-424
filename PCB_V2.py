cc659bbcd6a846a0a3dfbfe1233dd11ff7e1be6a
root <jacobgasser17@gmail.com>
2021-02-19 20:20:02 +0000

    pcb_2 started

--- PCB_V2.py
+++ PCB_V2.py
@@ -1,2 +1,12 @@
 class PCB2:
+	def __init__(self, parent):
+		self.parent = parent
+		self.first_child
+		self.younger_sibling
+		sle.folder_sibling
 
+	def create(self, p):
+		return true
+
+	def destroy(self, p):
+		return false
--- app.py
+++ app.py
@@ -3,13 +3,5 @@ import time
 import PCB_V1
 import PCB_V2
 
-def v1 ():
-    print ("V1")
 
-def v2 ():
-    print ("V2")
 
-def test_time():
-    print ("test time")
-
-v1()
