4daffaf3e1944a541d0ba6504fd44131c533671f
Jacob Gasser <jacobgasser17@gmail.com>
2021-02-19 12:55:56 -0600

    init

---
+++ PCB_V1.py
@@ -0,0 +1,13 @@
+#dependencies
+import llist 
+from llist import sllist, sllistnode
+
+class PCB1:
+    def __init__(self):
+        print("Self")
+
+    def create(self):
+        print("Self")
+
+    def destroy(self):
+        print("Self")
---
+++ PCB_V2.py
@@ -0,0 +1,9 @@
+class PCB2:
+    def __init__(self):
+        print("Self")
+
+    def create(self):
+        print("Self")
+
+    def destroy(self):
+        print("Self")
