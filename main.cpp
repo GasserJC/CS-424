2a2202886fed681cc13e808da672107d92b680ff
Jacob Gasser <jacobgasser17@gmail.com>
2021-03-10 15:37:41 -0600

    testing stage

--- main.cpp
+++ main.cpp
@@ -4,31 +4,31 @@
 #include <thread>  
 
     
-const int n = 100;
-const int k = 20;
-const int t = 4;
+const int N = 100;
+const int K = 20;
+const int T = 4;
 
-void mainThread(int* buffer, int kk, int tt){
+void mainThread(int* buffer, int k, int t){
     // start producer
     // start consumer
 
-    std::thread prod(&Producer::produce, &buffer, kk, tt);
-    std::thread cons(&Consumer::consume, &buffer, kk, tt);
+    std::thread prod(&Producer::produce, &buffer, k, t);
+    std::thread cons(&Consumer::consume, &buffer, k, t);
 
     prod.join();
     cons.join();
 }
 
 int main(){
-    int buffer [n];
+    int buffer [N];
 
     //initialize the buffer
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < N; i++){
         buffer[i] = 0;
     }
 
     //Call Main Thread
-    std::thread Host (mainThread, &buffer, k, t);
+    std::thread Host (mainThread, &buffer, K, T);
     std::cout << "Started Main Thread." << std::endl;
     Host.join();
     std::cout << "Finished Main Thread." << std::endl;
