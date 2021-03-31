3daf2a7ed122d09caa46ad0ceb05ca4394bac9aa
Jacob Gasser <jacobgasser17@gmail.com>
2021-03-31 13:58:28 -0500

    detect

--- detect.cpp
+++ detect.cpp
@@ -15,8 +15,15 @@ def HasSafeState():
 bool HasSafeState(std::string data[]){
        int resources = (int)data[0][0] - 48;
        int processes = (int)data[1][0] - 48;
-       std::cout << std::endl << "resources: " << resources << std::endl;
-       std::cout << std::endl << "processes: " << processes << std::endl;
+       int * Max = new int [processes*resources];
+       for(int i = 0; i < processes; i++){
+              for(int j = 0; j < resources; j++){
+                     int tmp = Max[i*processes + j] = (int)data[5+i][2*j] - 48;
+              }
+       }
+       for(int i = 0; i < 15; i++){
+              std::cout << std::endl << Max[i];
+       }
        return true;
 }
 
