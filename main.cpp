431df2b6e8a15abe16ebc769b1d84b136163ba85
Jacob Gasser <jacobgasser17@gmail.com>
2021-03-31 17:04:51 -0500

    detect

--- main.cpp
+++ main.cpp
@@ -76,6 +76,7 @@ int main(int argc, char* args[]){
         //Base
             //Quit
     } catch (...) {
+        std::cout << std::endl << "Data File Error!"  << std::endl;
         return -1;
     }
 }
