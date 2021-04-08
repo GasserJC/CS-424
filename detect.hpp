987cfbbef9b3a9806f5165adf494c5452be3f00c
Jacob Gasser <jacobgasser17@gmail.com>
2021-04-08 18:58:10 -0500

    Bankers

--- detect.hpp
+++ detect.hpp
@@ -23,9 +23,9 @@ int* GetNum(int line, std::string data[]){
        for(int i = 0; i < tmp.size() ; i++){
               RtrArr[i] = std::stoi(tmp[i]);
        }
-       std::cout << std::endl << "------" << std::endl;
+       std::cout << "------" << std::endl;
        for(int i = 0; i < tmp.size() ; i++){
-              std::cout << RtrArr[i] << " ";
+              std::cout << RtrArr[i] << ",";
        }
        std::cout<< std::endl <<"------" ;
        return RtrArr;
