fb8781036a14dedeed882a99620686a70f319914
Jacob Gasser <jacobgasser17@gmail.com>
2021-04-08 19:13:03 -0500

    Bankers

--- detect.hpp
+++ detect.hpp
@@ -73,7 +73,7 @@ int* GetMax(std::string data[]){
        std::cout << std::endl;
        //init max
        int* tmp;
-       for(int i = 0; i < PROCESSES; i++){
+       for(int i = 0; i <= PROCESSES; i++){
               tmp = GetNum(i+5, data);
               std::cout << std::endl << tmp[0] << "_" <<tmp[1] <<"_" << tmp[2]<< std::endl ;
               for(int j = 0; j < RESOURCES; j++){
