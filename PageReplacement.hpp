37ebdf498c1b9a010798dc94591ba0d4e9ebaa08
Jacob Gasser <jacobgasser17@gmail.com>
2021-04-24 09:55:09 -0500

    infrastructure

---
+++ PageReplacement.hpp
@@ -0,0 +1,21 @@
+#ifndef PAGE
+#define PAGE
+
+static const int LEFT = 1;
+static const int RIGHT = 6;
+static const int REF_SIZE = 20;
+static const int REF_AMP = 10;
+
+void FIFO(int * arr, int FC){
+
+}
+
+void LRU(int * arr, int FC){
+    
+}
+
+void OPT(int * arr, int FC){
+    
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,63 +1,80 @@
-#include "ProdCons.hpp"
+#include <iostream>
+#include "PageReplacement.hpp"
+#include <stdio.h>      /* printf, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
-/*
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-NOTE: BASH COMMAND TO RUN IS:       !
-                                    !
-$ g++ main.cpp -pthread             !
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-This File Contains Main Thread Logic and the Abstraction of the issue.
-    ProdCons.hpp => producer and consumer logic
-    Semaphore.hpp => Semaphore Mutex Class to prevent race conditions
-    Rando.hpp => A thread safe Random Number Generator
-
-!!!
-    You may find K, N, T in ProdCons.hpp
-!!!
-
-Race Condition Triggers:
-    Any values of k, N, T could cause a race condition.
-    If either process happens to be processing faster than another, (even if k = 1).
-    Then eventually that faster process will lap the other (ie: race condition)
-
-    Personally I used:
-    T = 3
-    K = 6
-    N = 12
-    and still got race conditions.
-
-*/
-
-void run(int buffer[], int n, int k, int t) {
-    try{
-        std::thread prod(Producer, std::ref(buffer), n, k, t);
-        std::thread cons(Consumer, std::ref(buffer), n, k, t);
-        cons.join();
-        prod.join();
-    } catch(...) {
-        std::cout << std::endl << "!! Terminated the Threads !!" << std::endl;
-    }
-}
-
-void mainThread(int buffer[], int n, int k, int t) {
-    std::thread MainT(run, std::ref(buffer), n, k, t);
-    MainT.join();
-}
-
-int main() {
-
-    int buffer[N];
-    //initialize the buffer
-    for (int i = 0; i < N; i++) {
-        buffer[i] = 0;
-    }
-    //Call Main Thread
-    std::cout << std::endl << "There is no auto-terminate!" << std:: endl;
-    std::cout << std::endl << "Starting Main Thread" << std:: endl;
-    mainThread(buffer, N, K, T);
-    std::cout << std::endl << "Finished Main Thread." << std::endl;
-
+int main(){
+    while(PageReplacement()){}
     return 1;
 }
 
+int GetPageCount(){
+
+    int tmp_page_frames;
+    std::cout << "How many page frames are in the system?";
+    std::cin >> tmp_page_frames;
+
+    if(tmp_page_frames < LEFT || tmp_page_frames > RIGHT){
+        tmp_page_frames = 0;
+    }
+
+    return tmp_page_frames;
+
+}
+
+int * GetRandRef(){
+    int tmp [REF_SIZE];
+    srand(time(NULL));
+    for(int i = 0; i < REF_SIZE; i++){
+        tmp[i] = rand() % REF_AMP;
+    }
+    return tmp;
+}
+
+void PrintArr(int * arr, int n){
+    std::cout << "[ ";
+    for(int i = 0; i < n; i++){
+        std::cout << arr[i];
+        if(i < n - 1){
+            std::cout << " , ";
+        } else {
+            std::cout << " ]";
+        }
+    }
+}
+
+bool PageReplacement(){
+    int FrameCount = 0;
+
+    //Set Page Frame Count
+    while(true){
+        FrameCount = GetPageCount();
+        if (FrameCount) { break; }
+    }
+
+    //generate string with page numbers
+    int * ReferenceString = GetRandRef();
+    PrintArr(ReferenceString, REF_SIZE);
+
+    //Display page faults using FIFO
+    FIFO(ReferenceString, FrameCount);
+
+    //Display page faults using LRU
+    LRU(ReferenceString, FrameCount);
+
+    //Display page faults using OPT
+    OPT(ReferenceString, FrameCount);
+
+    delete [] ReferenceString;
+
+    std::string tmp = "";
+    std::cout <<std::endl << "Do you want do another iteration of the program? (y/n): ";
+    std::cin >> tmp;
+
+    if(tmp == "y" || tmp == "Y" || tmp == "yes" || tmp == "Yes"){
+        return true;
+    } else {
+        return false;
+    }
+}
